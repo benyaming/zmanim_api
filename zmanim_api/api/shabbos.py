757f58cdd2de0f1e85d8254917eb2e8daf32f5e0
Benyamin Ginzburg <benyomin.94@gmail.com>
2020-03-25 23:27:17 +0200

    wip

--- zmanim_api/api/shabbos.py
+++ zmanim_api/api/shabbos.py
@@ -1,8 +1,8 @@
 from datetime import datetime as dt, timedelta, date as Date
 
-from pytz import timezone
 from zmanim.util.geo_location import GeoLocation
 from zmanim.zmanim_calendar import ZmanimCalendar
+
 
 from zmanim_api.helpers import HavdalaChoises
 from .utils import get_next_weekday, get_tz
@@ -34,43 +34,17 @@ async def shabbos(
         'tzeis_595_degrees': ('tzais', {'degrees': 5.95}),
     }
     havdala_calculator, kwargs = havdala_calculators[havdala.name]
+
     havdala_time: dt = getattr(saturday_calendar, havdala_calculator)(kwargs)
-
-
-
-    # tz_time = timezone(tz)
-    # now = dt.now(tz_time)
-    #
-    # # calculating nearest Saturday
-    # delta = timedelta(5 - now.weekday() % 7)
-    # shabbos_date = now + delta
-    # eve_date = shabbos_date - timedelta(1)
-    #
-    # eve_data = await get_calendar_data(tz, eve_date, lat, lng, cl_offset)
-    # shabbos_data = await get_calendar_data(tz, shabbos_date, lat, lng)
-    # parasha = _(txt.shabbos_names[shabbos_data['parsha_shabbos']])
-    #
-    # if not shabbos_data['zmanim']['sunset']:
-    #     final_data = {
-    #         'parasha': parasha,
-    #         'cl': None,
-    #         'cl_offset': None,
-    #         'tzeit_kochavim': None,
-    #         'warning': False,
-    #         'error': True
-    #     }
-    #     return final_data
-    #
-    # warning = True if not shabbos_data['zmanim']['alos_ma'] else False
+    late_cl_warning = False if friday_calendar.alos() else True
 
     final_data = {
-        # 'torah_part': '',  # todo
+        'torah_part': '',  # todo
         'cl': friday_calendar.candle_lighting(),
         'cl_offset': cl_offset,
         'havdala': havdala_time,
         'havdala_opinion': havdala.value,
-        # 'warning': warning,  # todo
-        # 'error': False  # todo
+        'late_cl_warning': late_cl_warning
     }
 
     return final_data

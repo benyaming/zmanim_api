7199686e4ab3c72c4147f94a4b3b74dad4dd8ff0
Benyamin Ginzburg <benyomin.94@gmail.com>
2020-09-24 18:23:20 +0300

    Fixed shabbat with yomtov case

--- test/test_engine/test_shabbat.py
+++ test/test_engine/test_shabbat.py
@@ -42,3 +42,88 @@ def test_shabbat_with_late_cl_warning():
     resp = get_shabbat(55.5, 37.7, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees,
                        date(2020, 6, 15))
     assert resp.dict(exclude_none=True, by_alias=True) == expected
+
+
+def test_shabbat_with_different_parsha():
+    expected_1 = {
+        'candle_lighting': dt.fromisoformat('2020-06-05T19:26:00+03:00'),
+        'havdala': dt.fromisoformat('2020-06-06T20:26:00+03:00'),
+        'settings': {
+            'date': date.fromisoformat('2020-05-30'),
+            'cl_offset': 18,
+            'havdala_opinion': 'tzeis_8_5_degrees',
+            'coordinates': (32.09, 34.86),
+            'elevation': 0
+        },
+        'torah_part': 'behaalosecha',
+        'late_cl_warning': False
+    }
+    expected_2 = {
+        'candle_lighting': dt.fromisoformat('2020-06-05T19:41:00+03:00'),
+        'havdala': dt.fromisoformat('2020-06-06T20:46:00+03:00'),
+        'settings': {
+            'date': date.fromisoformat('2020-05-30'),
+            'cl_offset': 18,
+            'havdala_opinion': 'tzeis_8_5_degrees',
+            'coordinates': (37.7, 34.86),
+            'elevation': 0
+        },
+        'torah_part': 'naso',
+        'late_cl_warning': False
+    }
+
+    resp_1 = get_shabbat(LAT, LNG, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees, date.fromisoformat('2020-05-30'))
+    resp_2 = get_shabbat(37.7, LNG, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees, date.fromisoformat('2020-05-30'))
+
+    assert resp_1.dict(exclude_none=True, by_alias=True) == expected_1
+    assert resp_2.dict(exclude_none=True, by_alias=True) == expected_2
+
+
+def test_shabbat_with_yomtov():
+    expected_1 = {
+        'candle_lighting': dt.fromisoformat('2020-05-29T19:22:00+03:00'),
+        'havdala': dt.fromisoformat('2020-05-30T20:22:00+03:00'),
+        'settings': {
+            'date': date.fromisoformat('2020-05-29'),
+            'cl_offset': 18,
+            'havdala_opinion': 'tzeis_8_5_degrees',
+            'coordinates': (32.09, 34.86),
+            'elevation': 0
+        },
+        'torah_part': 'naso',
+        'late_cl_warning': False
+    }
+    expected_2 = {
+        'candle_lighting': dt.fromisoformat('2020-05-29T19:36:00+03:00'),
+        'havdala': dt.fromisoformat('2020-05-30T20:41:00+03:00'),
+        'settings': {
+            'date': date.fromisoformat('2020-05-29'),
+            'cl_offset': 18,
+            'havdala_opinion': 'tzeis_8_5_degrees',
+            'coordinates': (37.7, 34.86),
+            'elevation': 0
+        },
+        'torah_part': 'shavuos',
+        'late_cl_warning': False
+    }
+    expected_3 = {
+        'candle_lighting': dt.fromisoformat('2020-04-10T18:53:00+03:00'),
+        'havdala': dt.fromisoformat('2020-04-11T19:52:00+03:00'),
+        'settings': {
+            'date': date.fromisoformat('2020-04-10'),
+            'cl_offset': 18,
+            'havdala_opinion': 'tzeis_8_5_degrees',
+            'coordinates': (37.7, 34.86),
+            'elevation': 0
+        },
+        'torah_part': 'chol_hamoed_pesach',
+        'late_cl_warning': False
+    }
+
+    resp_1 = get_shabbat(LAT, LNG, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees, date.fromisoformat('2020-05-29'))
+    resp_2 = get_shabbat(37.7, LNG, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees, date.fromisoformat('2020-05-29'))
+    resp_3 = get_shabbat(37.7, LNG, ZERO_ELEVATION, CL_OFFSET, HavdalaChoises.tzeis_8_5_degrees, date.fromisoformat('2020-04-10'))
+
+    assert resp_1.dict(exclude_none=True, by_alias=True) == expected_1
+    assert resp_2.dict(exclude_none=True, by_alias=True) == expected_2
+    assert resp_3.dict(exclude_none=True, by_alias=True) == expected_3

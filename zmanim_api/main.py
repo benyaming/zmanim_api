5411ac0d4e85c594a576a697419223367847c366
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-05-21 13:40:46 +0300

    Integration with betterlogging and sentry

--- zmanim_api/main.py
+++ zmanim_api/main.py
@@ -1,23 +1,36 @@
 import logging
 from os import getenv
 
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from betterlogging import get_colorized_logger, INFO
 
 from zmanim_api.api_helpers import DateException
-from zmanim_api.settings import ROOT_PATH
+from zmanim_api.settings import ROOT_PATH, SENTRY_PUBLIC_KEY
 from zmanim_api.routers.main_router import main_router
-from better_exceptions import logger
 
+
+logger = get_colorized_logger()
+logger.setLevel(INFO)
 
 app = FastAPI(root_path=f'/{ROOT_PATH}', docs_url='/', title='Zmanim API', version='1.0.1')
+
 app.include_router(main_router, tags=['Main'])
 
 
 @app.on_event('startup')
 async def on_start():  # pragma: no cover
     logger.info('STARTING ZMANIM API...')
+
+
+@app.middleware('http')  # pragma: no cover
+async def set_sentry_context(request: Request, call_next):
+    if SENTRY_PUBLIC_KEY:
+        sentry_sdk.set_context('request', dict(request))
+        sentry_sdk.set_user({'ip_address': request.client.host})
+    return await call_next(request)
 
 
 @app.exception_handler(DateException)
@@ -32,7 +45,8 @@ async def date_exception_handler(request: Request, exc: DateException):
 
 @app.exception_handler(Exception)  # pragma: no cover
 async def main_exception_handler(request: Request, e: Exception):
-    logger.exception(e)
+    sentry_sdk.capture_exception(e)
+
     return JSONResponse(
         status_code=500,
         content={'message': repr(e)}
@@ -45,12 +59,9 @@ if __name__ == '__main__':  # pragma: no cover
         host='0.0.0.0' if getenv('DOCKER_MODE') else '127.0.0.1',
         port=8000,
         use_colors=True,
-        log_level=logging.DEBUG
+        log_level=logging.DEBUG,
+        log_config='../uvicorn_logger.json'
     )
 
 # todo return zmanim calculation errors
-
 # todo translate: parshat hashavua names; daf yomi units;
-
-# C:\Users\Benyomin\PycharmProjects\zmanim_api>python c:\Users\Benyomin\AppData\Local\Programs\Python\Python36-32\Tools\i18n\pygettext.py -d zmanim_api -o zmanim_api\api\locales\base.pot zmanim_ap
-# i\api\localized_texts.py
